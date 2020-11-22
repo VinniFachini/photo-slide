38b2cb92a2412e37b6ee8dbb8ddbb4dcc749dea0
VinniFachini <vinniebrasil@gmail.com>
2020-11-22 16:39:16 -0300

    Slides configuration enhancement

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-import Slide from './slide.js'
+import Slide from './slide.js';
 
 const slide = new Slide('.slide', '.wrapper');
 
-slide.init()
+slide.init();
+console.log(slide);
+slide.changeSlide(3);
